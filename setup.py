26ba16b44896fbab555af1c46900a0f18b90bf04
Carlos Colon <carlos-eduardo.colon-rangel@hpe.com>
2018-08-09 14:35:27 -0500

    V1

---
+++ setup.py
@@ -0,0 +1,51 @@
+import sys
+import subprocess
+import string
+from setuptools import setup, find_packages
+
+sys.path.insert(0, '.')
+requirements = ['python-ldap', 'flask-wtf', 'flask-sqlalchemy', 'flask-login']
+packages = find_packages(  )
+
+__version__ = string.split('$0.0.0.1 $')[1]
+__date__ = string.join(string.split('$2018/08/09 14:00:00 $')[1:3], ' ')
+__author__ = 'Carlos Colon <espacio.sideral@gmail.com>'
+__build__ = subprocess.check_output('git describe --tags --always HEAD'.split()).decode().strip()
+
+with open('ldap_test/_version.py', 'w') as f:
+    f.write('''\
+# I will destroy any changes you make to this file.
+# Sincerely,
+# setup.py ;)
+__version__ = '{}'
+__date__ = '{}'
+__author__ = '{}'
+__build__ = '{}'
+'''.format(__version__, __date__, __author__, __build__))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ldap_test',
+    version=__version__,
+    packages=packages,
+    install_requires=requirements,
+    url='https://github.com/cppmx/ldap_test',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='BSD',
+    author='Carlos Colón',
+    author_email='espacio.sideral@gmail.com',
+    description='Ldap Test Login',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ),
+    options={
+        'sdist': {
+            'formats': ['gztar', 'zip'],
+        },
+    }
+)
